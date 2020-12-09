c60edc06e607e586a1c1bc974aab193c2d69a2f9
Suman11411 <49188734+Suman11411@users.noreply.github.com>
2020-12-09 23:06:41 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "PUBLIC_IP" {
-    value = aws_instance.App_server.public_ip
+    value = aws_instance.App_server1.public_ip
 }
 
 output "PRIVATE_IP" {
-    value = aws_instance.App_server.private_ip
+    value = aws_instance.App_server1.private_ip
 }
